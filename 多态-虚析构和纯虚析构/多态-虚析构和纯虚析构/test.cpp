31e181abc767bf5969a416dddbc1e80ee3a96e50
Ricky <2946677871@qq.com>
2021-02-09 17:24:56 +0800

    多态

---
+++ 多态-虚析构和纯虚析构/多态-虚析构和纯虚析构/test.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal
+{
+public:
+	Animal()
+	{
+		cout << "Animal构造函数调用" << endl;
+	}
+	virtual void speak() = 0;
+	virtual ~Animal() = 0;
+};
+Animal::~Animal()//纯虚析构函数必须要有实现
+{
+	cout << "Animal纯虚析构函数调用" << endl;
+}
+
+class Cat :public Animal
+{
+public:
+	Cat(string name)
+	{
+		cout << "Cat构造函数调用" << endl;
+		m_Name = new string(name);
+	}
+	void speak()
+	{
+		cout <<*m_Name<<"小猫在说话" << endl;
+	}
+	~Cat()
+	{
+		cout << "Cat析构函数调用" << endl;
+		if (this->m_Name != NULL)
+		{
+			delete m_Name;
+			m_Name = NULL;
+		}
+	}
+	string* m_Name;
+};
+
+void test01()
+{
+	Animal* abs =new Cat("Tom");
+	abs->speak();
+	delete abs;
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}

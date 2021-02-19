c86e0d9f559aff1ca311dbd7c3a145f4c3bf050f
Ricky <2946677871@qq.com>
2021-02-19 12:47:52 +0800

    函数对象-谓词

---
+++ 谓词-二元谓词/谓词-二元谓词/test.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class MySort
+{
+public:
+	bool operator()(int v1, int v2)
+	{
+		return v1 > v2;
+	}
+};
+void test01()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(40);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(50);
+	sort(v.begin(), v.end());
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+	sort(v.begin(), v.end(), MySort());
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}

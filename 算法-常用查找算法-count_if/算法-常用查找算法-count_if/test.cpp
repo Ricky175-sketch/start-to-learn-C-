2260f6bd0e510ff9c50867e4f22734afee66a079
Ricky <2946677871@qq.com>
2021-02-21 20:03:02 +0800

    常用算法-查找

---
+++ 算法-常用查找算法-count_if/算法-常用查找算法-count_if/test.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+class Greater4
+{
+public:
+	bool operator()(int val)
+	{
+		return val >= 4;
+	}
+};
+void test01()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(4);
+	v.push_back(5);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(4);
+	int num = count_if(v.begin(), v.end(), Greater4());
+	cout << num << endl;
+}
+class Person
+{
+public:
+	Person(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+	string m_Name;
+	int m_Age;
+};
+class Less35
+{
+public:
+	bool operator()(const Person& p)
+	{
+		return p.m_Age < 35;
+	}
+};
+void test02()
+{
+	vector<Person> v;
+	Person p1("¡ı±∏", 35);
+	Person p2("πÿ”", 35);
+	Person p3("’≈∑…", 35);
+	Person p4("’‘‘∆", 30);
+	Person p5("≤‹≤Ÿ", 25);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+	int num = count_if(v.begin(), v.end(), Less35());
+	cout << num << endl;
+}
+int main()
+{
+	test02();
+	system("pause");
+	return 0;
+}
